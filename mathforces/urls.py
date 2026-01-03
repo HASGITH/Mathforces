4cb8007ae7bbdab292666cb33981d91dc187912f
HASGITH <gasanhpvictus57@gmail.com>
2026-01-03 18:18:13 +0400

    Update URL patterns for admin and add redirect
    Added a redirect from the root URL to /archive/ and fixed the admin path.

--- mathforces/urls.py
+++ mathforces/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView # Добавь этот импорт
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.length),
     path('archive/', include('archive.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), # Добавь эту строку
+    path('accounts/', include('django.contrib.auth.urls')),
+    # Эта строка перенаправит пустой адрес на /archive/
+    path('', RedirectView.as_view(url='/archive/', permanent=True)),
 ]
