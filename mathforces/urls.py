71e11f144fb31b170271674580a915152a104f55
HASGITH <gasanhpvictus57@gmail.com>
2026-01-03 18:24:49 +0400

    Add missing RedirectView import

--- mathforces/urls.py
+++ mathforces/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # Исправлено здесь
     path('archive/', include('archive.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), # Добавь эту строку
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/archive/', permanent=True)),
 ]
