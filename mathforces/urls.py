58c805e020aaacadd73b0afcc17eb37a0c4994ca
HASGITH <gasanhpvictus57@gmail.com>
2026-01-03 18:31:24 +0400

    Update urls.py to final version

--- mathforces/urls.py
+++ mathforces/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('archive/', include('archive.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # Добавь эту строку
+    path('', RedirectView.as_view(url='/archive/', permanent=True)),
 ]
